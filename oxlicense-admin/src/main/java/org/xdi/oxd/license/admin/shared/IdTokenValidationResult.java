bbbf42f437d83cbda2dd254a6fb1671e68f9ceb2
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-07-10 00:52:54 +0300

    restricting access to license admin

---
+++ oxlicense-admin/src/main/java/org/xdi/oxd/license/admin/shared/IdTokenValidationResult.java
@@ -0,0 +1,12 @@
+package org.xdi.oxd.license.admin.shared;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 10/07/2016
+ */
+
+public enum IdTokenValidationResult {
+    ACCESS_GRANTED,
+    NOT_IN_LICENSE_GROUP,
+    CANT_PARSE
+}
