05a7097f91dc4dbe571491bec3b6b447a9c21770
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-07-15 11:07:38 +0300

    added generate licent ID client  (provides both vclient API for java and testing ability)

--- oxlicense-client/src/main/java/org/xdi/oxd/license/client/GenerateWS.java
+++ oxlicense-client/src/main/java/org/xdi/oxd/license/client/GenerateWS.java
@@ -1,14 +1,11 @@
 package org.xdi.oxd.license.client;
 
 import org.xdi.oxd.license.client.data.LicenseResponse;
+import org.xdi.oxd.license.client.js.LicenseMetadata;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -27,5 +24,12 @@ public interface GenerateWS {
     @Path("/generate")
     @Produces(MediaType.APPLICATION_JSON)
     LicenseResponse generatePost(@FormParam("licenseId") String licenseId);
+
+    @POST
+    @Path("/generateLicenseId/{licenseCount}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response generateLicenseId(@PathParam("licenseCount") int licenseCount,
+                                      @HeaderParam("Authorization") String rpt,
+                                      LicenseMetadata licenseMetadata);
 
 }
