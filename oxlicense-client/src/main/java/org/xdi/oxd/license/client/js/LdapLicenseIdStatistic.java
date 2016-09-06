8db139c9cc3b1f2179a5e718af9c06524805d275
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-09-06 11:13:50 +0300

    added new oxLicenseIdStatistic OC and removed unused customer OC

---
+++ oxlicense-client/src/main/java/org/xdi/oxd/license/client/js/LdapLicenseIdStatistic.java
@@ -0,0 +1,76 @@
+package org.xdi.oxd.license.client.js;
+
+/**
+ * @author Yuriy Zabrovarnyy
+ * @version 0.9, 06/09/2016
+ */
+
+import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
+import org.gluu.site.ldap.persistence.annotation.LdapDN;
+import org.gluu.site.ldap.persistence.annotation.LdapEntry;
+import org.gluu.site.ldap.persistence.annotation.LdapObjectClass;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@LdapEntry
+@LdapObjectClass(values = {"top", "'oxLicenseIdStatistic'"})
+public class LdapLicenseIdStatistic implements Serializable {
+
+    @LdapDN
+    private String dn;
+    @LdapAttribute(name = "uniqueIdentifier")
+    private String uniqueIdentifier;
+    @LdapAttribute(name = "oxCreationDate")
+    private Date creationDate;
+    @LdapAttribute(name = "oxMacAddress")
+    private String macAddress;
+
+    public String getDn() {
+        return dn;
+    }
+
+    public void setDn(String dn) {
+        this.dn = dn;
+    }
+
+    public String getUniqueIdentifier() {
+        return uniqueIdentifier;
+    }
+
+    public void setUniqueIdentifier(String uniqueIdentifier) {
+        this.uniqueIdentifier = uniqueIdentifier;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LdapLicenseIdStatistic that = (LdapLicenseIdStatistic) o;
+
+        return !(uniqueIdentifier != null ? !uniqueIdentifier.equals(that.uniqueIdentifier) : that.uniqueIdentifier != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return uniqueIdentifier != null ? uniqueIdentifier.hashCode() : 0;
+    }
+}
