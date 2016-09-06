759f9ad9f0939a5a0b5bbf790aa25dbda62f170c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-09-06 15:05:25 +0300

    caught bug in persistence

--- oxlicense-client/src/main/java/org/xdi/oxd/license/client/js/LdapLicenseIdStatistic.java
+++ oxlicense-client/src/main/java/org/xdi/oxd/license/client/js/LdapLicenseIdStatistic.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 @LdapEntry
-@LdapObjectClass(values = {"top", "'oxLicenseIdStatistic'"})
+@LdapObjectClass(values = {"top", "oxLicenseIdStatistic"})
 public class LdapLicenseIdStatistic implements Serializable {
 
     @LdapDN
